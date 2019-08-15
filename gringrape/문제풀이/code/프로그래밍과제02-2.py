1d2ca4c22fc646eaf8dfae9f1329f5c9ab2d6ad7
jaemin93 <icebanana93@gmail.com>
2019-08-15 20:42:20 +0900

    homework_02_02 review #1

--- gringrape/문제풀이/code/프로그래밍과제02-2.py
+++ gringrape/문제풀이/code/프로그래밍과제02-2.py
@@ -10,7 +10,21 @@ def ceiling(data, start, end, K):
                 return middle
         else:
             return ceiling(data, middle + 1, end, K)
-            
+
+# jaemin
+'''
+def ceiling(data, start, end, K):
+    middle = int((start + end) / 2)
+    if start > end:
+        return -1
+    else:
+        if data[middle] >= K and data[middle - 1] >= K:
+            return ceiling(data, start, middle - 1, K)
+        elif data[middle] >= K:
+            return middle
+        else:
+            return ceiling(data, middle + 1, end, K)
+'''
 
 N = 25
 data = [1, 3, 6, 9, 13, 17, 21, 23, 24, 31, 37, 38, 44, 45, 47, 51, 55, 58, 71, 73, 88, 91, 99, 101, 102]
